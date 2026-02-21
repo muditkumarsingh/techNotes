2fc72e9a5d7bbcb687fb6272a325b3766608727e
Mudit Kumar Singh <muditksingh96184@gmail.com>
2026-02-21 22:53:59 +0530

    Vercel error

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import { setCredentials } from "../../features/auth/authSlice"
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://technotes-api-cvmn.onrender.com',
+    baseUrl: 'https://tech-notes-dyx9g7bj2-mudit-kumar-singhs-projects.vercel.app',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
